c0ef7d9a55bb1c55256eeef0be70052896cb0645
Charles Paradiso <carlos.gonzalez@paradisosolutions.com>
2017-08-26 14:46:48 -0400

    Configuration values
    Config file in order to set the configuration options to create the
    communicate both systems.

---
+++ configuration/default.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// Global Configuration
+$cnf['apikey'] = 'c94778cf35d2430c0af1511ee281efad';
+$cnf['lang'] = 'en';
+$cnf['timezone'] = 'Europe/Berlin';
+$cnf['adult'] = false;
+$cnf['debug'] = false;
+
+// Data Return Configuration - Manipulate if you want to tune your results
+$cnf['appender']['movie'] = array('trailers', 'images', 'credits', 'translations', 'reviews');
+$cnf['appender']['tvshow'] = array('trailers', 'images', 'credits', 'translations', 'keywords');
+$cnf['appender']['season'] = array('trailers', 'images', 'credits', 'translations');
+$cnf['appender']['episode'] = array('trailers', 'images', 'credits', 'translations');
+$cnf['appender']['person'] = array('movie_credits', 'tv_credits', 'images');
+$cnf['appender']['collection'] = array('images');
+$cnf['appender']['company'] = array('movies');
+
+?>
